755561fb6f00831e74ccf6005b074abc2c5bb6a5
anshumalivfx <anshumali.karna99@gmail.com>
2022-05-22 14:32:14 +0530

    Manage

---
+++ CodeDriver.h
@@ -0,0 +1,10 @@
+//
+// Created by Anshumali Karna on 22/05/22.
+//
+
+#ifndef AGO_PROJECT_CODEDRIVER_H
+#define AGO_PROJECT_CODEDRIVER_H
+#import <iostream>
+void DriverCode ();
+
+#endif //AGO_PROJECT_CODEDRIVER_H
--- main.cpp
+++ main.cpp
@@ -1,30 +1,12 @@
 //
 // Created by Anshumali Karna on 22/05/22.
 //
-#import "TicTacToe.h"
+#import "CodeDriver.h"
 using namespace std;
 
 int main()
 {
-	printf("\n-------------------------------------------------------------------\n\n");
-	printf("\t\t\t Tic-Tac-Toe\n");
-	printf("\n-------------------------------------------------------------------\n\n");
-	char cont='y';
-	do {
-		char choice;
-	 	printf("Do you want to start first?(y/n) : ");
-	 	scanf(" %c", &choice);
-
-		if(choice=='n')
-			playTicTacToe(COMPUTER);
-		else if(choice=='y')
-			playTicTacToe(HUMAN);
-		else
-			printf("Invalid choice\n");
-
-		printf("\nDo you want to quit(y/n) : ");
-       		scanf(" %c", &cont);
-	} while(cont=='n');
+    DriverCode();
 	return (0);
 }
 
