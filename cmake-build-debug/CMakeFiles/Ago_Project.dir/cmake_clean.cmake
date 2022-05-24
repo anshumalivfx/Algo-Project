646b2a52d9c35ea95041d7a20db568b1b398442c
anshumalivfx <anshumali.karna99@gmail.com>
2022-05-24 09:28:51 +0530

    HAHAHAHHA

---
+++ cmake-build-debug/CMakeFiles/Ago_Project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Ago_Project"
+  "Ago_Project.pdb"
+  "CMakeFiles/Ago_Project.dir/CodeDriver.cpp.o"
+  "CMakeFiles/Ago_Project.dir/CodeDriver.cpp.o.d"
+  "CMakeFiles/Ago_Project.dir/TicTacToe.cpp.o"
+  "CMakeFiles/Ago_Project.dir/TicTacToe.cpp.o.d"
+  "CMakeFiles/Ago_Project.dir/main.cpp.o"
+  "CMakeFiles/Ago_Project.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ago_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
